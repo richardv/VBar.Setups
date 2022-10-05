264c01763eb3ad79e0af1f0772157d1e6190000f
RichardV <richardvanaken@hotmail.com>
2022-10-05 19:55:20 +0100

    Tests

---
+++ src/VBar.Setups.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace VBar.Setups.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
